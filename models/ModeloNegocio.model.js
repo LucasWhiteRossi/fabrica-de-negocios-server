210736cd712c4b4fa402dccf34c9a072e8bf2a98
Priscila Castro <priscilacastroa@gmail.com>
2022-05-16 17:25:35 -0300

    continuacaoNegocio

--- models/ModeloNegocio.model.js
+++ models/ModeloNegocio.model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const modeloNegocioSchema = new Schema({
     owner:  {type: mongoose.Types.ObjectId, ref: "User", required:true},
-    vinculoNegocio:  {type: mongoose.Types.ObjectId, ref: "ModeloNegocio"},
+    vinculoPersona: [{type: mongoose.Types.ObjectId, ref: "Persona"}],
     beneficio: { type: String },
     solucao: { type: String },
     produtos: { type: String },
